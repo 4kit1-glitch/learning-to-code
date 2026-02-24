0b0456146cdb27a601564370eb8cf5136742b141
Kit <4jrkit1@gmail.com>
2026-02-24 11:18:46 +0100

    added some exercises

---
+++ rtrig.py
@@ -0,0 +1,11 @@
+
+import math
+
+x = 14
+
+v = math.pow(math.cos(x), 2) + math.pow(math.sin(x),3)  #value of the trig rule
+
+print(round(v))
+
+x= 2
+print(math.e ** 2 , math.pow(math.e , 2) ,math.exp(x) )
