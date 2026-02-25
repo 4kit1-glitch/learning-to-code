a799ffb42c0f3ca876ba9ba9e97d9e8994ea80c5
Kit <4jrkit1@gmail.com>
2026-02-25 09:19:33 +0100

    added rect.py

--- while learning/jupy/rect.py
+++ while learning/jupy/rect.py
@@ -17,3 +17,11 @@ def rectangle(lenght, width):
     for i in range(2):
         polyline(n = 1 , angle=90 , lenght = lenght)
         polyline(n = 1 , angle = 90 , lenght = width)
+
+def rhombus(side_lenght , internal_angle):
+    linear_angle =  180 -  internal_angle   ### to draw a 180 degree line 
+    for i in range(2):
+        polyline(n=1 , angle = internal_angle , lenght = side_lenght)
+        polyline(n = 1 , angle = linear_angle ,lenght= side_lenght)
+
+
