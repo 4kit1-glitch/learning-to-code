9559b06d36b40474e9fee909859c3554e8950775
Kit <4jrkit1@gmail.com>
2026-03-03 22:00:57 +0100

    added test file for the strip method

---
+++ chapter8/strip.py
@@ -0,0 +1,9 @@
+# testing behavior of the strip method
+# The strip method for strings removes whatever its argument is
+# from the string provided that the argument is found either at
+# The end or begining of the function
+
+message = "hello, wordh"
+
+print(message,"hello")
+print(message.strip('h'),"heloo")
