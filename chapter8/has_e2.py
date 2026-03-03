ff675a30c4225a7f8a1b5738312d7422a2257594
Kit <4jrkit1@gmail.com>
2026-03-03 22:38:41 +0100

    revised som e algorithms

--- chapter8/has_e2.py
+++ chapter8/has_e2.py
@@ -1,7 +1,10 @@
 #better algorithm
 
+# def has_e(word):
+#     return 'e' in word or 'E' in word 
+
 def has_e(word):
-    return 'e' in word or 'E' in word
+    return 'e' in word.lower()  # very efficient algorithm
 
 print(has_e("hello"))
 print(has_e("nappy"))
