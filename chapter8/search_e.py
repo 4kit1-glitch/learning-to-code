037cb7239edc98d8591e7e62f093c0a33e9e5e4a
Kit <4jrkit1@gmail.com>
2026-03-03 23:27:19 +0100

    added doctest

--- chapter8/search_e.py
+++ chapter8/search_e.py
@@ -1,5 +1,16 @@
 
+from doctest import run_docstring_examples
+
+def run_docsrings(func):
+    run_docstring_examples(func, globals(), name=func.__name__)
+
 def has_e(word):
+    """Checks if a word has the letter e
+    >>> has_e('happy')
+    False
+    >>> has_e('help')
+    True
+    """
     return 'e' in word.lower()
 
 file_object = open('chapter8/words.txt')
@@ -10,5 +21,4 @@ for line in file_object:
         print(line.strip())
         num = num + 1
 
-print(f"There are {num} lines that use e")
-        
+# print(f"There are {num} lines that use e")
