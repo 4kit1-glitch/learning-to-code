18c22305ef09805da6b75507765985d0f4f6610a
Kit <4jrkit1@gmail.com>
2026-03-03 21:48:15 +0100

    modified read.py with strip function

--- chapter8/read.py
+++ chapter8/read.py
@@ -1,4 +1,9 @@
 
 file_object = open('chapter8/words.txt')
 
-print(file_object.readline())
+line = file_object.readline()
+word = line.strip()
+word2 = file_object.readline().strip()
+print(word, word2)
+
+# print(file_object.readline() + file_object.readline())
