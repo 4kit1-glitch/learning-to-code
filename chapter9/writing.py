53bdc6db7f9430d4cc6a95d40238fa6f67967b7a
Kit <4jrkit1@gmail.com>
2026-03-07 10:51:04 +0100

    feat : added object closers

--- chapter9/writing.py
+++ chapter9/writing.py
@@ -7,9 +7,20 @@ def special_lines(file_object):
         if line.startswith('***'):
             writer.write(line)
     writer.close()
+    reader.close()
     
 def write(file_object):
     for i in range(3):
         word = input("enter your 3 friends names: ")
         writer.write(word + f"{len(word)}\n")
     writer.close()
+
+def read(file_object):
+    for line in file_object:
+        if len(line) > 0:
+            print(line.strip())
+    file_object.close()
+
+
+write(writer)
+read(open('chapter9/written.txt'))
