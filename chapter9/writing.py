d4c48b826751d63dcfda8f09469a46b55b018a48
Kit <4jrkit1@gmail.com>
2026-03-07 10:37:23 +0100

    feat : added object closers

--- chapter9/writing.py
+++ chapter9/writing.py
@@ -6,12 +6,10 @@ def special_lines(file_object):
     for line in file_object:
         if line.startswith('***'):
             writer.write(line)
-
+    writer.close()
+    
 def write(file_object):
     for i in range(3):
         word = input("enter your 3 friends names: ")
         writer.write(word + f"{len(word)}\n")
-
-
-
-write(writer)
+    writer.close()
