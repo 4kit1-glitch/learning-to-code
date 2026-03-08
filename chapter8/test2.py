9e296b2584bc294ae442ae4a09fa80051509a57f
Kit <4jrkit1@gmail.com>
2026-03-08 20:06:17 +0100

    feat : testing re tools

---
+++ chapter8/test2.py
@@ -0,0 +1,15 @@
+# testing the ^ and $ special characters
+
+from re import search
+reader = open('chapter8/words.txt')
+
+def count_pattern(file_object):
+    count = 0
+    pattern = "^c"
+    for line in file_object:
+        result = search(pattern, line.lower())
+        if result != None:
+            count += 1
+    return count
+
+print(count_pattern(reader))
