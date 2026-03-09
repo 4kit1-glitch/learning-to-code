bf9ae47f4a8d2c425d2eef4984ecdc2190b86a2f
Kit <4jrkit1@gmail.com>
2026-03-09 09:32:09 +0100

    working with w3schools

---
+++ w3schools/string.py
@@ -0,0 +1,16 @@
+text = "why are you up this late"
+word = "Jockerr"
+
+#capitalize
+print(text.capitalize())
+
+#casefole
+print(word.casefold())
+
+#center
+print(word.center(6))
+
+#count
+print(text.count(" ", 4, 12),)
+
+#encode
