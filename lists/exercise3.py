c02427c1830d97f67fd8254f179bebd2812775d5
Kit <4jrkit1@gmail.com>
2026-03-09 19:07:58 +0100

    exercises

--- lists/exercise3.py
+++ lists/exercise3.py
@@ -1,2 +1,7 @@
-def reversed_sentence(sentence):
+def reverse_sentence(sentence):
     word_list = sentence.split()
+    reversed_list = list(reversed(word_list))
+    reversed_sentence = ' '.join(reversed_list).capitalize()
+    return reversed_sentence
+
+print(reverse_sentence("Reverse this sentence"))
