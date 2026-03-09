1ca4a5022630d71f3a65f664e9f3754351bd87ed
Kit <4jrkit1@gmail.com>
2026-03-09 18:35:53 +0100

    exercises

--- lists/exercise2.py
+++ lists/exercise2.py
@@ -2,3 +2,14 @@ def is_palindrome(word):
     if list(reversed(word)) == list(word):
         return True
     return False
+
+def is_palindrome2(word):
+    delimiter = ''
+    if delimiter.join(reversed(word)) == word:
+        return True
+    return False
+
+def is_palindrome3(word):
+    if word[::-1] == word:
+        return True
+    return False
