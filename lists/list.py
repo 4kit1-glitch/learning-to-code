5e8d40052770b61d66262b637dd3ee876854ec4b
Kit <4jrkit1@gmail.com>
2026-03-11 11:24:26 +0100

    testing

---
+++ lists/list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 22:38:11 2026
+
+@author: aimmee
+"""
+
+# numbers = list(map(int, input("enter two numbers:(num1,num2): ").split(',')))
+
+numbers = [int(i) for i in input("enter the numbers:").split()]
