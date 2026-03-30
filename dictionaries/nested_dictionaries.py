4af0c38ddbb92190e75b05fb8161cf804939cc78
Kit <4jrkit1@gmail.com>
2026-03-30 09:46:58 +0100

    working with nested dicts

--- dictionaries/nested_dictionaries.py
+++ dictionaries/nested_dictionaries.py
@@ -18,3 +18,26 @@ family = {
         "hobby": "singing"
     }
 }
+
+# method 2
+child1 = {
+    "name": "harisson",
+    "age": 4
+}
+child2 = {
+    "name": "harisson",
+    "age": 4
+}
+
+childern = {
+    "child1": child1,
+    "child2": child2
+}
+
+# working with them
+print(family.get("father").get("name"))
+
+# looping with nested dicts
+for key, value1 in family.items():
+    for value in value1.values():
+        print(key, value)
