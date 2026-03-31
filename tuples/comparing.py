295019d7dbfb063b151622af2d916f81034d5013
Kit <4jrkit1@gmail.com>
2026-03-31 19:15:23 +0100

    feat: sorting

---
+++ tuples/comparing.py
@@ -0,0 +1,4 @@
+if (1, 3, 4) < (1, 3):
+    print(True)
+else:
+    print(False)
---
+++ tuples/sort.py
@@ -0,0 +1,16 @@
+members = {
+    "boys": 10,
+    "girls": 2,
+    "born":5
+}
+member_count = [(key, value) for key, value in members.items()]
+
+def second_element(x):
+    return x[1]
+
+#before sort
+print(member_count)
+
+#after sort
+member_count.sort(key = second_element)
+print(member_count)
